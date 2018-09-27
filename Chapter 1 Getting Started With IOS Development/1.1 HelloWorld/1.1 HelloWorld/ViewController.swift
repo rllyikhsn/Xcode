cce15e88016489647f72566ea7c791195469c9b2
Rully Ikhsan <angledark68@gmail.com>
2018-09-27 17:53:51 +0700

    Membuat Hello World

---
+++ Chapter 1 Getting Started With IOS Development/1.1 HelloWorld/1.1 HelloWorld/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  1.1 HelloWorld
+//
+//  Created by rully ikhsan on 27/09/18.
+//  Copyright © 2018 rully. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Mencetak Text pada console
+        print("Sukses Membuat Aplikasi IOS Pertama")
+    }
+
+
+}
+
