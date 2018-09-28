6cd4df185da0ec2a3b46ae3c1149ec65ade38aae
Rully Ikhsan <angledark68@gmail.com>
2018-09-28 15:04:00 +0700

    Membuat Form Login

---
+++ LoginForm/LoginForm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginForm
+//
+//  Created by rully ikhsan on 28/09/18.
+//  Copyright © 2018 rully. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
